e14df75a5819bc61daa9cc4b3f50a907e73ce390
seongsan <tjdtks030@naver.com>
2024-10-09 21:36:59 +0900

    chore: Service Exception 작성 및 이메일 인증 관련 StatusCode 추가

---
+++ app/src/main/java/com/developeek/circleon/data/exception/ServiceException.kt
@@ -0,0 +1,13 @@
+package com.developeek.circleon.data.exception
+
+import java.io.IOException
+
+object ServiceException {
+    /**
+     * NoResultException
+     *
+     * 통신 결과 컨텐츠가 없는 경우 발생하는 Exception
+     * 경우에 따라 Critical 정도가 달라지기 때문에 Repository 에서 취할 action 을 구분하기 위해 제작
+     */
+    class NoResultException(override val message: String) : IOException(message)
+}
