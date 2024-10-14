561f97fdc8a1d7daa90e3f17c3c7a60666d953b4
seongsan <tjdtks030@naver.com>
2024-10-14 11:43:25 +0900

    design: 로그인 화면 레이아웃 및 관련 values 작성

---
+++ app/src/main/java/com/developeek/circleon/view/screen/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.developeek.circleon.view.screen
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.developeek.circleon.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
