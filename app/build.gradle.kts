f062f8cbed21b820284ceadcbe8a6b6ddbe42de0
seongsan <tjdtks030@naver.com>
2024-10-24 13:55:40 +0900

    feat: bottom nav back key 편의성 기능 추가 및 홈 화면 레이아웃 일부 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.developeek.circleon"
-        minSdk = 29
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/developeek/circleon/view/screen/HomeActivity.kt
+++ app/src/main/java/com/developeek/circleon/view/screen/HomeActivity.kt
@@ -1,13 +1,27 @@
 package com.developeek.circleon.view.screen
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.developeek.circleon.R
 import com.developeek.circleon.databinding.ActivityHomeBinding
+import com.developeek.circleon.view.screen.calendar.CalendarFragment
+import com.developeek.circleon.view.screen.circle.CircleFragment
+import com.developeek.circleon.view.screen.directmessage.DirectMessageFragment
+import com.developeek.circleon.view.screen.home.HomeFragment
+import com.developeek.circleon.view.screen.login.LoginActivity
+import com.developeek.circleon.view.screen.mypage.MyPageFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private var backClicked = false
+    private var onMainFragment = true
+    private lateinit var finishWaitingToast: Toast
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +29,85 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // TODO: access 토큰 갱신 결과에 따라 로그인 화면 분기하도록 수정
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
+        initBottomNav()
+        initFinishWaitingToast()
+    }
+
+    private fun initBottomNav() {
+        add(HomeFragment())
+        supportFragmentManager.addFragmentOnAttachListener { _, fragment ->
+            onMainFragment = fragment is HomeFragment
+        }
+
+        binding.btmNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navHome -> replaceTo(HomeFragment())
+                R.id.navCalendar -> replaceTo(CalendarFragment())
+                R.id.navCircle -> replaceTo(CircleFragment())
+                R.id.navDirectMessage -> replaceTo(DirectMessageFragment())
+                R.id.navMyPage -> replaceTo(MyPageFragment())
+            }
+
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun add(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(binding.flHome.id, fragment)
+        transaction.commit()
+    }
+
+    private fun replaceTo(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(binding.flHome.id, fragment)
+        transaction.commit()
+    }
+
+    private fun initFinishWaitingToast() {
+        finishWaitingToast =
+            Toast.makeText(this, FINISH_WAITING_MESSAGE, Toast.LENGTH_SHORT).also {
+                it.addCallback(
+                    object : Toast.Callback() {
+                        override fun onToastShown() {
+                            backClicked = true
+                            super.onToastShown()
+                        }
+
+                        override fun onToastHidden() {
+                            backClicked = false
+                            super.onToastHidden()
+                        }
+                    },
+                )
+            }
+    }
+
+    override fun onKeyDown(
+        keyCode: Int,
+        event: KeyEvent?,
+    ): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (!onMainFragment) {
+                replaceTo(HomeFragment())
+                binding.btmNav.selectedItemId = R.id.navHome
+                return true
+            } else {
+                if (backClicked) {
+                    finish()
+                } else {
+                    finishWaitingToast.show()
+                    return true
+                }
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    companion object {
+        private const val FINISH_WAITING_MESSAGE = "종료를 원하시면 '뒤로'버튼을 한번 더 눌러주세요"
     }
 }
