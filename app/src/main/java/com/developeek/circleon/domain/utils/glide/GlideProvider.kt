59461c4bc0fac2f41506040c4ee6453559c1e894
seongsan <tjdtks030@naver.com>
2024-11-19 10:12:40 +0900

    fix: GlideProvider 확장자에 따라 contentType 적용
    getImageExtension() 추가

--- app/src/main/java/com/developeek/circleon/domain/utils/glide/GlideProvider.kt
+++ app/src/main/java/com/developeek/circleon/domain/utils/glide/GlideProvider.kt
@@ -22,12 +22,14 @@ class GlideProvider
             var glideUrl: GlideUrl? = null
 
             tokenManager.getAccessToken()?.let {
+                val imageExtension = getImageExtension(url)
+
                 glideUrl =
                     GlideUrl(
-                        url.substring(0, url.length - 5),
+                        url.substring(0, url.length - imageExtension.length - 1),
                         LazyHeaders.Builder()
                             .addHeader(AUTHORIZATION, "Bearer $it")
-                            .addHeader(CONTENT_TYPE, "image/jpeg")
+                            .addHeader(CONTENT_TYPE, "image/$imageExtension")
                             .build(),
                     )
             }
@@ -45,8 +47,22 @@ class GlideProvider
             }
         }
 
+        private fun getImageExtension(url: String): String {
+            var symbolIdx = 0
+
+            for (i in (url.length - 1) downTo 0) {
+                if (url[i] == EXTENSION_SYMBOL) {
+                    symbolIdx = i
+                    break
+                }
+            }
+
+            return url.substring(symbolIdx + 1, url.length)
+        }
+
         companion object {
             private const val AUTHORIZATION = "Authorization"
             private const val CONTENT_TYPE = "Content-Type"
+            private const val EXTENSION_SYMBOL = '.'
         }
     }
