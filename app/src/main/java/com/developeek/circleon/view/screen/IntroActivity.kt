a50ace638191690f915590d03415f7b4a9d2675c
seongsan <tjdtks030@naver.com>
2025-07-24 16:59:43 +0900

    design: mipmap 아이콘 수정

--- app/src/main/java/com/developeek/circleon/view/screen/IntroActivity.kt
+++ app/src/main/java/com/developeek/circleon/view/screen/IntroActivity.kt
@@ -2,13 +2,17 @@ package com.developeek.circleon.view.screen
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.developeek.circleon.R
 import com.developeek.circleon.databinding.ActivityIntroBinding
 import com.developeek.circleon.view.screen.home.HomeActivity
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
+    private val splashDisplayLength = 500L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,12 @@ class IntroActivity : AppCompatActivity() {
         installSplashScreen()
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        startActivity(Intent(this, HomeActivity::class.java))
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this, HomeActivity::class.java))
+                overridePendingTransition(androidx.appcompat.R.anim.abc_fade_in, androidx.appcompat.R.anim.abc_fade_out)
+            },
+            splashDisplayLength,
+        )
     }
 }
